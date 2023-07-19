a235c07c90f282af7d6232e244edd4a38df4384b
Michahide <73429490+Michahide@users.noreply.github.com>
2023-07-20 05:37:16 +0700

    first-commit

---
+++ automation-testing/myinfo_module.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+@pytest.fixture()
+
+def driver():
+    driver = webdriver.Chrome()
+    driver.get('https://opensource-demo.orangehrmlive.com/')
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+def test(driver):
+    time.sleep(3)
+    driver.find_element(By.NAME, 'username').send_keys('Admin')
+    driver.find_element(By.NAME, 'password').send_keys('admin123' + Keys.ENTER)
+    time.sleep(3)
+
+    # Masuk_Menu_My_Info 
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]').click()
+    time.sleep(3)
+
+    # Mengupdate_Personal_Details 
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[1]').click()
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[1]/div/div/div[2]/div[3]/div[2]/input').send_keys(Keys.CONTROL, "a", Keys.DELETE)
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[1]/div/div/div[2]/div[3]/div[2]/input').send_keys('Bambang')
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div[2]/div/div/div[2]/input').send_keys('Seema')
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button').click()
+    time.sleep(3)
+    #Tidak bisa pilih dropdown Blood Type
+
+    # Mengupdate_Contact_Details 
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[2]').click()
+    time.sleep(3)
+    # Tidak bisa pilih dropdown Country
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div/div[1]/div/div[2]/input').send_keys('0251 123456')
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/button').click()
+    time.sleep(3)
